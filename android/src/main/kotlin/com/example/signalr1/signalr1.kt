4dc6f7dcd191fc0930fa0409507026d380bdba99
monikhopade <74231020+monikhopade@users.noreply.github.com>
2021-02-08 14:08:20 +0530

    Update signalr1.kt

--- android/src/main/kotlin/com/example/signalr1/signalr1.kt
+++ android/src/main/kotlin/com/example/signalr1/signalr1.kt
@@ -9,24 +9,25 @@ import microsoft.aspnet.signalr.client.hubs.HubProxy
 import microsoft.aspnet.signalr.client.transport.LongPollingTransport
 import microsoft.aspnet.signalr.client.transport.ServerSentEventsTransport
 
-enum class CallMethod1(val value: String) {
+enum class CallMethod(val value: String) {
     ConnectToServer1("connectToServer1"),
     Reconnect1("reconnect1"),
     Stop1("stop1"),
     ListenToHubMethod1("listenToHubMethod1"),
-    InvokeServerMethod1("invokeServerMethod1")
-}
-enum class CallMethod(val value: String) {
+    InvokeServerMethod1("invokeServerMethod1"),
     ConnectToServer("connectToServer"),
     Reconnect("reconnect"),
     Stop("stop"),
     ListenToHubMethod("listenToHubMethod"),
     InvokeServerMethod("invokeServerMethod")
 }
-object SignalR {
+
+
+object SignalR1 {
     private lateinit var connection: HubConnection
     private lateinit var hub: HubProxy
-
+    private lateinit var connection1: HubConnection
+    private lateinit var hub1: HubProxy
     fun connectToServer(url: String, hubName: String, queryString: String, headers: Map<String, String>, transport: Int, hubMethods: List<String>, result: Result) {
         try {
             connection = if (queryString.isEmpty()) {
@@ -148,86 +149,7 @@ object SignalR {
             result.error("Error", ex.localizedMessage, null)
         }
     }
-}
-object SignalR1 {
-    private lateinit var connection1: HubConnection
-    private lateinit var hub1: HubProxy
-
-    fun connectToServer(url: String, hubName: String, queryString: String, headers: Map<String, String>, transport: Int, hubMethods: List<String>, result: Result) {
-        try {
-            connection1 = if (queryString.isEmpty()) {
-                HubConnection(url)
-            } else {
-                HubConnection(url, queryString, true, Logger { _: String, _: LogLevel ->
-                })
-            }
-
-            if (headers.isNotEmpty()) {
-                val cred = Credentials() { request ->
-                    request.headers = headers
-                }
-                connection1.credentials = cred
-            }
-            hub1 = connection1.createHubProxy(hubName)
-
-            hubMethods.forEach { methodName ->
-                hub1.on(methodName, { res ->
-                    android.os.Handler(Looper.getMainLooper()).post {
-                        Signalr1Plugin.channel1.invokeMethod("NewMessage", listOf(methodName, res))
-                    }
-                }, Any::class.java)
-            }
-
-            connection1.connected {
-                android.os.Handler(Looper.getMainLooper()).post {
-                    Signalr1Plugin.channel1.invokeMethod("Connection1Status", connection1.state.name)
-                }
-            }
-
-            connection1.reconnected {
-                android.os.Handler(Looper.getMainLooper()).post {
-                    Signalr1Plugin.channel1.invokeMethod("Connection1Status", connection1.state.name)
-                }
-            }
-
-            connection1.reconnecting {
-                android.os.Handler(Looper.getMainLooper()).post {
-                    Signalr1Plugin.channel1.invokeMethod("Connection1Status", connection1.state.name)
-                }
-            }
-
-            connection1.closed {
-                android.os.Handler(Looper.getMainLooper()).post {
-                    Signalr1Plugin.channel1.invokeMethod("ConnectionStatus", connection1.state.name)
-                }
-            }
-
-            connection1.connectionSlow {
-                android.os.Handler(Looper.getMainLooper()).post {
-                    Signalr1Plugin.channel1.invokeMethod("Connection1Status", "Slow")
-                }
-            }
-
-            connection1.error { handler ->
-                android.os.Handler(Looper.getMainLooper()).post {
-                    Signalr1Plugin.channel1.invokeMethod("Connection1Status", handler.localizedMessage)
-                }
-            }
-
-            when (transport) {
-                1 -> connection1.start(ServerSentEventsTransport(connection1.logger))
-                2 -> connection1.start(LongPollingTransport(connection1.logger))
-                else -> {
-                    connection1.start()
-                }
-            }
-
-            result.success(true)
-        } catch (ex: Exception) {
-            result.error("Error", ex.localizedMessage, null)
-        }
-    }
-
+    
     fun connectToServer1(url: String, hubName: String, queryString: String, headers: Map<String, String>, transport: Int, hubMethods: List<String>, result: Result) {
         try {
             connection1 = if (queryString.isEmpty()) {
